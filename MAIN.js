1349388654eef40eba75ee38bf1c3a845f6026fe
AQEEL1007 <aqeelmohamedazwer@gmail.com>
2024-02-11 13:05:24 +0300

    modified: HOME PAGE.css

--- MAIN.js
+++ MAIN.js
@@ -4,3 +4,28 @@ if (goldRateShape) {
     mover(goldRateShape, 0.7);
 }
 
+$(document).ready(function() {
+    // Open and close nav
+    $('#navbar-toggle').click(function(e) {
+        e.preventDefault();
+        $('nav ul').slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    // Toggle dropdown menus
+    $('nav ul li').click(function(e) {
+        if ($(this).children('.navbar-dropdown').length > 0) {
+            e.preventDefault();
+            $(this).children('.navbar-dropdown').slideToggle("slow");
+        }
+    });
+
+    // Close dropdowns when clicking outside of nav
+    $(document).click(function(e) {
+        if (!$(e.target).closest('nav').length && !$(e.target).is('#navbar-toggle')) {
+            $('nav ul').slideUp("slow");
+            $('#navbar-toggle').removeClass('active');
+            $('.navbar-dropdown').slideUp("slow");
+        }
+    });
+});
